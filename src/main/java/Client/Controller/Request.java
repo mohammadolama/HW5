291bf43f638afd8fdcd8b4b535a7bd5767bb190a
mohammadolama <rastegar123456789@gmail.com>
2020-07-25 14:18:02 +0430

    first commit

--- src/main/java/Client/Controller/Request.java
+++
@@ -1,9 +0,0 @@
-package Client.Controller;
-
-import Client.Model.AuthToken;
-
-import java.net.Socket;
-
-public interface Request {
-    public <T> T excute(AuthToken authToken, String value1 , String value2);
-}
